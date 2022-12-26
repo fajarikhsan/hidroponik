a8e3696c94c03dd201d0bdd4253d18ad09fb4f68
fajarikhsan <fajarikhsan3@gmail.com>
2022-12-26 08:42:03 +0700

    add logo

--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -31,9 +31,9 @@
         <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/home">
+                <div class="sidebar-brand-icon">
+                    <img src="/img/logo.jpg" alt="Logo" width="30%">
                 </div>
                 <!-- <div class="sidebar-brand-text mx-3"></div> -->
             </a>
